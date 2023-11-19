434538e1b106b71faa665ba89f46052c37bb7e17
Muzzamil104612 <145114127+Muzzamil104612@users.noreply.github.com>
2023-11-20 03:19:39 +0500

    Update add_student.php

--- add_student.php
+++ add_student.php
@@ -2,7 +2,6 @@
 
 include 'db_connnection.php';
 
-// If data is coming as JSON
 $data = json_decode(file_get_contents("php://input"), true);
 
 if (!isset($data['student_name']) || !isset($data['city']) || !isset($data['student_id'])) {
@@ -22,7 +21,6 @@ $student_id = (int)$data['student_id'];
 $student_name = mysqli_real_escape_string($conn, $data['student_name']);
 $city = mysqli_real_escape_string($conn, $data['city']);
 
-// Use prepared statements for security
 $query = "INSERT INTO students (student_id, student_name, city) VALUES (?, ?, ?)";
 $stmt = $conn->prepare($query);
 
@@ -31,10 +29,8 @@ $stmt->bind_param("iss", $student_id, $student_name, $city);
 if ($stmt->execute()) {
     echo json_encode(array('message' => "Data added successfully for student_id='.$student_id.', student_name='.$student_name.', and city='.$city"));
 } else {
-    // Log the detailed error on the server
     error_log("Error executing query: " . $query . " Error: " . $conn->error);
 
-    // Provide a generic error message to the client
     echo json_encode(array('error' => "Failed to add data. Please try again later."));
 }
 
